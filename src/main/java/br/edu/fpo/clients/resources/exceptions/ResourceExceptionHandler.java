52dd29e7be0ed3dfd1b3ea136441cba8c0443d61
BRUNO <bs01bs@gmail.com>
2021-05-17 18:25:24 -0300

    Alterad data sql and add annotation in resource exception handler

--- src/main/java/br/edu/fpo/clients/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/edu/fpo/clients/resources/exceptions/ResourceExceptionHandler.java
@@ -6,11 +6,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.edu.fpo.clients.services.exceptions.DatabaseException;
 import br.edu.fpo.clients.services.exceptions.ResourceNotFoundException;
 
+@ControllerAdvice
 public class ResourceExceptionHandler{
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
